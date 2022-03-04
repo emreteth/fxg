dd05ea046dd3c3113f1485371672d86d4d41afde
emreteth <81780199+emreteth@users.noreply.github.com>
2022-03-04 22:47:32 +0300

    Add files via upload

---
+++ dnm.py
@@ -0,0 +1,10 @@
+ort1 = int(input("1.sınav : "))
+ort2 = int(input("2.sınav : "))
+ort3 = int(input("1.sözlü : "))
+ort4 = int(input("2.sözlü : "))
+
+
+ort5 = ort1+ort2+ort3+ort4
+ort6 = ort5/4
+
+print("\nortalama ",ort6)
